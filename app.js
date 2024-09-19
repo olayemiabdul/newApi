38e90c9dcf816a131069689afc04b0dc9d6479c4
your-github-olayemiabdul <your-github-olayemi.abdullahi@gmail.com>
2024-09-19 18:02:27 +0100

    add VERB

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ let products = [
     name: 'Product A',
     description: 'Description for Product A',
     price: 29.99,
+    quantity:10,
     imageUrl: 'https://nadstore-1c5d66f32aff.herokuapp.com/images/app1.png'
   },
   {
@@ -28,6 +29,7 @@ let products = [
     name: 'Product B',
     description: 'Description for Product B',
     price: 49.99,
+    quantity:9,
     imageUrl: 'https://nadstore-1c5d66f32aff.herokuapp.com/images/app2.png'
   },
   {
@@ -35,6 +37,7 @@ let products = [
     name: 'Product C',
     description: 'Description for Product C',
     price: 19.99,
+    quantity:10,
     imageUrl: 'https://nadstore-1c5d66f32aff.herokuapp.com/images/app3.png'
   }
 ];
@@ -46,6 +49,7 @@ let shoes = [
     name: 'Shoe A',
     description: 'Description for Shoe A',
     price: 29.99,
+    quantity:10,
     imageUrl: 'https://nadstore-1c5d66f32aff.herokuapp.com/images/nike1.jpg' // Full URL
   },
   {
@@ -53,6 +57,7 @@ let shoes = [
     name: 'Shoe B',
     description: 'Description for Shoe B',
     price: 49.99,
+    quantity:10,
     imageUrl: 'https://nadstore-1c5d66f32aff.herokuapp.com/images/nike2.jpg' // Full URL
   },
   {
@@ -60,6 +65,7 @@ let shoes = [
     name: 'Shoe C',
     description: 'Description for Shoe C',
     price: 19.99,
+    quantity:10,
     imageUrl: 'https://nadstore-1c5d66f32aff.herokuapp.com/images/nike1.jpg' // Full URL
   }
 ];
@@ -81,6 +87,134 @@ app.get('/products/:id', (req, res) => {
   if (!product) return res.status(404).json({ error: 'Product not found' });
   res.json(product);
 });
+app.get('/shoes/:id', (req, res) => {
+  const product = shoes.find(p => p.id === parseInt(req.params.id));
+  if (!product) return res.status(404).json({ error: 'Product not found' });
+  res.json(product);
+});
+app.post("/products", (req, res) => {
+  const newId = lastId += 1;
+  const post = {
+    id: newId,
+
+    title: req.body.id,
+    content: req.body.name,
+    author: req.body.price,
+    quantity:req.body.quantity,
+    imageUrl: req.body.imageUrl,
+    date: new Date(),
+  };
+  lastId = newId;
+  products.push(post);
+  res.status(201).json(post);
+});
+
+app.post("/shoes", (req, res) => {
+  const newId = lastId += 1;
+  const post = {
+    id: newId,
+    title: req.body.id,
+    content: req.body.name,
+    description:req.body.description,
+    author: req.body.price,
+    quantity:req.body.quantity,
+    imageUrl: req.body.imageUrl,
+    date: new Date(),
+  };
+  lastId = newId;
+  shoes.push(post);
+  res.status(201).json(post);
+});
+
+// PATCH a post when you just want to update one parameter
+app.patch("/products/:id", (req, res) => {
+  const post = posts.find((p) => p.id === parseInt(req.params.id));
+  if (!post) return res.status(404).json({ message: "Post not found" });
+
+  if (req.body.id) post.id = req.body.id;
+  if (req.body.name) post.name = req.body.name;
+  if (req.body.description) post.description = req.body.description;
+  if (req.body.price) post.price = req.body.price;
+  if (req.body.quantity)post.quantity=req.body.quantity;
+  if (req.body.imageUrl) post.imageUrl = req.body.imageUrl;
+  
+
+  res.json(post);
+});
+
+app.patch("/shoes/:id", (req, res) => {
+  const post = shoes.find((p) => p.id === parseInt(req.params.id));
+  if (!post) return res.status(404).json({ message: "Post not found" });
+
+  if (req.body.id) post.id = req.body.id;
+  if (req.body.name) post.name = req.body.name;
+  if (req.body.description) post.description = req.body.description;
+  if (req.body.price) post.price = req.body.price;
+  if (req.body.quantity)post.quantity=req.body.quantity;
+  if (req.body.imageUrl) post.imageUrl = req.body.imageUrl;
+  
+
+  res.json(post);
+});
+//Put a product
+app.put("/products/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const replacementProduct = {
+    id: id,
+    name:req.body.name,
+    description:req.body.description,
+    price:req.body.price,
+    quantity:req.body.quantity,
+    imageUrl:req.body.imageUrl,
+
+    
+  };
+
+  const searchIndex = jokes.findIndex((joke) => joke.id === id);
+
+  jokes[searchIndex] = replacementProduct;
+   console.log(products);
+  res.json(replacementJoke);
+});
+
+app.put("/shoes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const replacementShoe = {
+    id: id,
+    name:req.body.name,
+    description:req.body.description,
+    price:req.body.price,
+    quantity:req.body.quantity,
+    imageUrl:req.body.imageUrl,
+
+    
+  };
+
+  const searchIndex = jokes.findIndex((joke) => joke.id === id);
+
+  jokes[searchIndex] = replacementShoe;
+   console.log(products);
+  res.json(replacementJoke);
+});
+
+// DELETE a specific post by providing the post id
+app.delete("/products/:id", (req, res) => {
+  const index = products.findIndex((p) => p.id === parseInt(req.params.id));
+  if (index === -1) return res.status(404).json({ message: "Post not found" });
+
+  products.splice(index, 1);
+  res.json({ message: "Post deleted" });
+});
+
+app.delete("/shoes/:id", (req, res) => {
+  const index = shoes.findIndex((p) => p.id === parseInt(req.params.id));
+  if (index === -1) return res.status(404).json({ message: "Post not found" });
+
+  shoes.splice(index, 1);
+  res.json({ message: "Post deleted" });
+});
+
+
 
 // Start the server
 app.listen(port, () => {
